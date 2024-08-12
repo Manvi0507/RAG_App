2de8c126d7564931bc69a58e0f802228834fa063
Manvi0507 <manvisharmagn@gmail.com>
2024-08-12 12:02:19 +0530

    Update ttcrag.py

--- ttcrag.py
+++ ttcrag.py
@@ -5,7 +5,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
@@ -47,10 +47,10 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-# Function to get Vector Store using Chroma
+# Function to get Vector Store using FAISS
 def get_vector_store(text_chunks):
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = Chroma.from_texts(text_chunks, embedding=embeddings, persist_directory="chromadb")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
 
 # Function to initialize conversational chain
@@ -176,8 +176,8 @@ def main():
        st.session_state.user_question = ""  # Clear the input cell
        st.session_state.response = ""  # Clear the output cell
        #st.session_state.memory = ConversationBufferMemory(
-         #  memory_key="history",
-          # input_key="question")  # Clear the memory
+          # memory_key="history",
+           #input_key="question")  # Clear the memory
 
     if user_question:
         st.session_state.response = user_question
