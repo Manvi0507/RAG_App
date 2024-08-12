690b535413f8081c96ec2b3ed6996bf4365f6ff7
Manvi0507 <manvisharmagn@gmail.com>
2024-08-12 11:53:31 +0530

    Update ttcrag.py

--- ttcrag.py
+++ ttcrag.py
@@ -5,7 +5,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import Chromadb
+from langchain.vectorstores import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
